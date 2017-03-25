50f4a3d71e5d21bea7810b1d91cb6ada95127768
sredeb01 <sre.deb01@gmail.com>
2017-03-25 22:36:01 +0530

    form validation

--- client/js/controller.js
+++ client/js/controller.js
@@ -1,74 +1,48 @@
-app.controller('gridgameCtrl', function($scope, $interval , $uibModal) {
+app.controller('formCtrl', function($scope, $http, $uibModal) {
 
-    $scope.shuffleArray =function(array){
-        for (var i =0; i<array.length; i++) {
-            var j = Math.floor(Math.random() * (i + 1));
-            var temp = array[i].colorIndex;
-            array[i].colorIndex = array[j].colorIndex;
-            array[j].colorIndex = temp;
-        }
-        return array;
-    }
-    $scope.allcolors = [];
-    $scope.timer;
-    $scope.setup = function(){
-        $scope.colors = ['red', 'green', 'blue', 'yellow'];
-        for(var i=0; i<16;i++){
-            $scope.allcolors.push({
-                isVisible: false,
-                isMatched: false,
-                gridIndex: i,
-                colorIndex: (i+1) % 4
-            });
-        }
-        $scope.allcolors = $scope.shuffleArray($scope.allcolors);
-        $scope.oldGrid = {color : null};
-        $scope.scoreMatch = 0;
-        $scope.timeOut = 60;
-    }
-    $scope.updateTime =function(){
-        if($scope.timeOut >0){
-            $scope.timeOut--;
-        }else{
-            alert("Time Out! Please try again later.")
-        }
-    }
-    $scope.addColor = function(grid){
-        console.log(grid);
-        console.log($scope.allcolors);
-        var selectedGrids = $scope.allcolors.filter(function(x){
-            return x.isVisible && !x.isMatched? true: false;
+    $scope.searchBtn = function(){
+        $scope.item ={};
+        var urlPrefix = 'https://randomform.herokuapp.com';
+        $http({
+            method: 'GET',
+            url: urlPrefix,
+            config: {
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
+                'Access-Control-Allow-Origin': 'flights.makemytrip.com'
+            }
+        }).then( function(response) {
+            $scope.item = response.data.data;
+            console.log($scope.item);
+            $scope.showDiv = true;
+            $scope.hideDiv = true;
         });
-        if(selectedGrids.length > 1){
-            selectedGrids.forEach(function(x){
-                console.log(x);
-                $scope.allcolors[x.gridIndex].isVisible = false;
-            });
-            $scope.allcolors[grid.gridIndex].isVisible = true;
-        }
-        else {
-            $scope.allcolors[grid.gridIndex].isVisible = true;
-            if(selectedGrids.length === 1) {
-                if(selectedGrids[0].colorIndex === grid.colorIndex) {
-                    $scope.allcolors[selectedGrids[0].gridIndex].isMatched = true;
-                    $scope.allcolors[grid.gridIndex].isMatched = true;  
-                    $scope.scoreMatch ++;
-                    if($scope.scoreMatch == 8){
-                        $uibModal.open({
+    }
+    $scope.submitForm = function(){
+       console.log("hi");
+        var url = 'https://randomform.herokuapp.com/submit';
+        $http({
+            method:'POST',
+            url:url,
+             headers: {
+                'Content-Type': 'application/json'
+            },
+            data: { bar: 'foo',
+                    foo:'bar'
+                 }
+        }) .then( function(res) {
+            if(res.data.success == true){
+                 $scope.showDiv = !true;
+                $scope.hideDiv = !true;
+                if ($scope.myForm.$valid) {
+                     $uibModal.open({
                             templateUrl: "winPopup",
                             size: "sm"
                         });
-                        $interval.cancel($scope.timer);
-                    }
                 }
             }
-        }
+        });      
     }
-    $scope.startGmae = function(){
-            $scope.hideDiv = true;
-            $scope.timer = $interval($scope.updateTime, 1000);
-    };
-    $scope.tiles = $scope.setup($scope.colors);
-
+   
 });
 	
--- readme.md
+++ readme.md
@@ -1,2 +1,9 @@
-Board Game
-4x4 Grid"# BoardGame" 
+You're required to create a single page web application which has a "GET FORM"
+
+button. Upon clicking the button a request should be made to the form api and
+
+render the form using the information received in the response. Once the form
+
+is rendered, the user must be able to "SUBMIT FORM". And also the user should
+
+be allowed to repeat this process.
